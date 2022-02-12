38aff90da96dc277c49213dca1ebae6daeda93a0
Pratik28Satdeve <partricksatdeve@gmail.com>
2022-02-12 20:53:14 +0530

    Pratik Added Code

---
+++ assetsp/js/script.js
@@ -0,0 +1,258 @@
+function rplus1() {
+
+    var item = parseInt(document.getElementById("fc1").innerHTML);
+    item++;
+    document.getElementById("fc1").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc1").innerHTML);
+    tot = 150 * item;
+    document.getElementById("ta1").innerHTML = tot;
+}
+
+function rminus1() {
+
+    var item = parseInt(document.getElementById("fc1").innerHTML);
+    item--;
+    document.getElementById("fc1").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc1").innerHTML);
+    tot = 150 * item;
+    document.getElementById("ta1").innerHTML = tot;
+}
+
+function rplus2() {
+
+    var item = parseInt(document.getElementById("fc2").innerHTML);
+    item++;
+    document.getElementById("fc2").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc2").innerHTML);
+    tot = 200 * item;
+    document.getElementById("ta2").innerHTML = tot;
+}
+
+function rminus2() {
+
+    var item = parseInt(document.getElementById("fc2").innerHTML);
+    item--;
+    document.getElementById("fc2").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc2").innerHTML);
+    tot = 200 * item;
+    document.getElementById("ta2").innerHTML = tot;
+}
+
+function rplus3() {
+
+    var item = parseInt(document.getElementById("fc3").innerHTML);
+    item++;
+    document.getElementById("fc3").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc3").innerHTML);
+    tot = 200 * item;
+    document.getElementById("ta3").innerHTML = tot;
+}
+
+function rminus3() {
+
+    var item = parseInt(document.getElementById("fc3").innerHTML);
+    item--;
+    document.getElementById("fc3").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc3").innerHTML);
+    tot = 200 * item;
+    document.getElementById("ta3").innerHTML = tot;
+}
+
+function rplus4() {
+
+    var item = parseInt(document.getElementById("fc4").innerHTML);
+    item++;
+    document.getElementById("fc4").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc4").innerHTML);
+    tot = 120 * item;
+    document.getElementById("ta4").innerHTML = tot;
+}
+
+function rminus4() {
+
+    var item = parseInt(document.getElementById("fc4").innerHTML);
+    item--;
+    document.getElementById("fc4").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc4").innerHTML);
+    tot = 120 * item;
+    document.getElementById("ta4").innerHTML = tot;
+}
+
+function rplus5() {
+
+    var item = parseInt(document.getElementById("fc5").innerHTML);
+    item++;
+    document.getElementById("fc5").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc5").innerHTML);
+    tot = 90 * item;
+    document.getElementById("ta5").innerHTML = tot;
+}
+
+function rminus5() {
+
+    var item = parseInt(document.getElementById("fc5").innerHTML);
+    item--;
+    document.getElementById("fc5").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc5").innerHTML);
+    tot = 90 * item;
+    document.getElementById("ta5").innerHTML = tot;
+}
+
+function rplus6() {
+
+    var item = parseInt(document.getElementById("fc6").innerHTML);
+    item++;
+    document.getElementById("fc6").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc6").innerHTML);
+    tot = 70 * item;
+    document.getElementById("ta6").innerHTML = tot;
+}
+
+function rminus6() {
+
+    var item = parseInt(document.getElementById("fc6").innerHTML);
+    item--;
+    document.getElementById("fc6").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc6").innerHTML);
+    tot = 70 * item;
+    document.getElementById("ta6").innerHTML = tot;
+}
+
+function rplus7() {
+
+    var item = parseInt(document.getElementById("fc7").innerHTML);
+    item++;
+    document.getElementById("fc7").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc7").innerHTML);
+    tot = 60 * item;
+    document.getElementById("ta7").innerHTML = tot;
+}
+
+function rminus7() {
+
+    var item = parseInt(document.getElementById("fc7").innerHTML);
+    item--;
+    document.getElementById("fc7").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc7").innerHTML);
+    tot = 60 * item;
+    document.getElementById("ta7").innerHTML = tot;
+}
+
+function rplus8() {
+
+    var item = parseInt(document.getElementById("fc8").innerHTML);
+    item++;
+    document.getElementById("fc8").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc8").innerHTML);
+    tot = 70 * item;
+    document.getElementById("ta8").innerHTML = tot;
+}
+
+function rminus8() {
+
+    var item = parseInt(document.getElementById("fc8").innerHTML);
+    item--;
+    document.getElementById("fc8").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc8").innerHTML);
+    tot = 70 * item;
+    document.getElementById("ta8").innerHTML = tot;
+}
+
+function rplus9() {
+
+    var item = parseInt(document.getElementById("fc9").innerHTML);
+    item++;
+    document.getElementById("fc9").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc9").innerHTML);
+    tot = 80 * item;
+    document.getElementById("ta9").innerHTML = tot;
+}
+
+function rminus9() {
+
+    var item = parseInt(document.getElementById("fc9").innerHTML);
+    item--;
+    document.getElementById("fc9").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc9").innerHTML);
+    tot = 80 * item;
+    document.getElementById("ta9").innerHTML = tot;
+}
+
+function rplus10() {
+
+    var item = parseInt(document.getElementById("fc10").innerHTML);
+    item++;
+    document.getElementById("fc10").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc10").innerHTML);
+    tot = 130 * item;
+    document.getElementById("ta10").innerHTML = tot;
+}
+
+function rminus10() {
+
+    var item = parseInt(document.getElementById("fc10").innerHTML);
+    item--;
+    document.getElementById("fc10").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc10").innerHTML);
+    tot = 130 * item;
+    document.getElementById("ta10").innerHTML = tot;
+}
+
+function rplus11() {
+
+    var item = parseInt(document.getElementById("fc11").innerHTML);
+    item++;
+    document.getElementById("fc11").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc11").innerHTML);
+    tot = 250 * item;
+    document.getElementById("ta11").innerHTML = tot;
+}
+
+function rminus11() {
+
+    var item = parseInt(document.getElementById("fc11").innerHTML);
+    item--;
+    document.getElementById("fc11").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc11").innerHTML);
+    tot = 250 * item;
+    document.getElementById("ta11").innerHTML = tot;
+}
+
+function rplus12() {
+
+    var item = parseInt(document.getElementById("fc12").innerHTML);
+    item++;
+    document.getElementById("fc12").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc12").innerHTML);
+    tot = 50 * item;
+    document.getElementById("ta12").innerHTML = tot;
+}
+
+
+function rminus12() {
+
+    var item = parseInt(document.getElementById("fc12").innerHTML);
+    item--;
+    document.getElementById("fc12").innerHTML = item;
+    var tot = parseInt(document.getElementById("fc12").innerHTML);
+    tot = 50 * item;
+    document.getElementById("ta12").innerHTML = tot;
+}
+
+function amount() {
+    var item1 = parseInt(document.getElementById("ta1").innerHTML)
+    var item2 = parseInt(document.getElementById("ta2").innerHTML)
+    var item3 = parseInt(document.getElementById("ta3").innerHTML)
+    var item4 = parseInt(document.getElementById("ta4").innerHTML)
+    var item5 = parseInt(document.getElementById("ta5").innerHTML)
+    var item6 = parseInt(document.getElementById("ta6").innerHTML)
+    var item7 = parseInt(document.getElementById("ta7").innerHTML)
+    var item8 = parseInt(document.getElementById("ta8").innerHTML)
+    var item9 = parseInt(document.getElementById("ta9").innerHTML)
+    var item10 = parseInt(document.getElementById("ta10").innerHTML)
+    var item11 = parseInt(document.getElementById("ta11").innerHTML)
+    var item12 = parseInt(document.getElementById("ta12").innerHTML)
+    var total = item1 + item2 + item3 + item4 + item5 + item6 + item7 + item8 + item9 + item10 + item11 + item12;
+
+    document.getElementById("total").innerHTML = total;
+}
