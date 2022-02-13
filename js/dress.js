23506159f0555702ee2e67f3ba491de716556796
sushilwaghe <sushilwaghe2169@gmail.com>
2022-02-13 15:34:14 +0530

    add: js code

--- js/dress.js
+++ js/dress.js
@@ -1,40 +1,195 @@
-// function dec(){
-//     let currentQuantity= document.getElementById('qnt-dress').innerHTML;
-//     if(currentQuantity>0)
-//     {
-//         document.getElementById('qnt-dress').innerHTML = --currentQuantity;
-//     }
-// }
-// function inc(){
+function dec(){
+    var currentQuantity= parseInt(document.getElementById('dress').innerHTML);
+    if(currentQuantity>0)
+    {
+        document.getElementById('dress').innerHTML = --currentQuantity;
+    }
+}
+function inc(){
 
-//     let currentQuantity= document.getElementById('qnt-dress').innerHTML;
+    var currentQuantity= parseInt(document.getElementById('dress').innerHTML);
 
-//     document.getElementById('qnt-dress').innerHTML = ++currentQuantity;
-//     }
+    document.getElementById('dress').innerHTML = ++currentQuantity;
+    }
 
-    const productOptions = document.querySelectorAll(".card-order");
-    productOptions.forEach((node) => {
-         const itemCount = node.querySelector(".qnt-dress");
+    function dec1(){
+        var currentQuantity= parseInt(document.getElementById('dress1').innerHTML);
+        if(currentQuantity>0)
+        {
+            document.getElementById('dress1').innerHTML = --currentQuantity;
+        }
+    }
+    function inc1(){
+    
+        var currentQuantity= parseInt(document.getElementById('dress1').innerHTML);
+    
+        document.getElementById('dress1').innerHTML = ++currentQuantity;
+        }
+
+        
+    function dec2(){
+        var currentQuantity= parseInt(document.getElementById('dress2').innerHTML);
+        if(currentQuantity>0)
+        {
+            document.getElementById('dress2').innerHTML = --currentQuantity;
+        }
+    }
+    function inc2(){
+    
+        var currentQuantity= parseInt(document.getElementById('dress2').innerHTML);
+    
+        document.getElementById('dress2').innerHTML = ++currentQuantity;
+        }
+
+        function dec3(){
+            var currentQuantity= parseInt(document.getElementById('dress3').innerHTML);
+            if(currentQuantity>0)
+            {
+                document.getElementById('dress3').innerHTML = --currentQuantity;
+            }
+        }
+        function inc3(){
+        
+            var currentQuantity= parseInt(document.getElementById('dress3').innerHTML);
+        
+            document.getElementById('dress3').innerHTML = ++currentQuantity;
+            }
+
+            function dec4(){
+                var currentQuantity= parseInt(document.getElementById('dress4').innerHTML);
+                if(currentQuantity>0)
+                {
+                    document.getElementById('dress4').innerHTML = --currentQuantity;
+                }
+            }
+            function inc4(){
+            
+                var currentQuantity= parseInt(document.getElementById('dress4').innerHTML);
+            
+                document.getElementById('dress4').innerHTML = ++currentQuantity;
+                }
+
+                function dec5(){
+                    var currentQuantity= parseInt(document.getElementById('dress5').innerHTML);
+                    if(currentQuantity>0)
+                    {
+                        document.getElementById('dress5').innerHTML = --currentQuantity;
+                    }
+                }
+                function inc5(){
+                
+                    var currentQuantity= parseInt(document.getElementById('dress5').innerHTML);
+                
+                    document.getElementById('dress5').innerHTML = ++currentQuantity;
+                    }
+
+                    function dec6(){
+                        var currentQuantity= parseInt(document.getElementById('dress6').innerHTML);
+                        if(currentQuantity>0)
+                        {
+                            document.getElementById('dress6').innerHTML = --currentQuantity;
+                        }
+                    }
+                    function inc6(){
+                    
+                        var currentQuantity= parseInt(document.getElementById('dress6').innerHTML);
+                    
+                        document.getElementById('dress6').innerHTML = ++currentQuantity;
+                        }
+
+                        function dec7(){
+                            var currentQuantity= parseInt(document.getElementById('dress7').innerHTML);
+                            if(currentQuantity>0)
+                            {
+                                document.getElementById('dress7').innerHTML = --currentQuantity;
+                            }
+                        }
+                        function inc7(){
+                        
+                            var currentQuantity= parseInt(document.getElementById('dress7').innerHTML);
+                        
+                            document.getElementById('dress7').innerHTML = ++currentQuantity;
+                            }
+
+                            function dec8(){
+                                var currentQuantity= parseInt(document.getElementById('dress8').innerHTML);
+                                if(currentQuantity>0)
+                                {
+                                    document.getElementById('dress8').innerHTML = --currentQuantity;
+                                }
+                            }
+                            function inc8(){
+                            
+                                var currentQuantity= parseInt(document.getElementById('dress8').innerHTML);
+                            
+                                document.getElementById('dress8').innerHTML = ++currentQuantity;
+                                }
+
+                                function dec9(){
+                                    var currentQuantity= parseInt(document.getElementById('dress9').innerHTML);
+                                    if(currentQuantity>0)
+                                    {
+                                        document.getElementById('dress9').innerHTML = --currentQuantity;
+                                    }
+                                }
+                                function inc9(){
+                                
+                                    var currentQuantity= parseInt(document.getElementById('dress9').innerHTML);
+                                
+                                    document.getElementById('dress9').innerHTML = ++currentQuantity;
+                                    }
+
+                                    function dec10(){
+                                        var currentQuantity= parseInt(document.getElementById('dress10').innerHTML);
+                                        if(currentQuantity>0)
+                                        {
+                                            document.getElementById('dress10').innerHTML = --currentQuantity;
+                                        }
+                                    }
+                                    function inc10(){
+                                    
+                                        var currentQuantity= parseInt(document.getElementById('dress10').innerHTML);
+                                    
+                                        document.getElementById('dress10').innerHTML = ++currentQuantity;
+                                        }
+
+                                        function dec11(){
+                                            var currentQuantity= parseInt(document.getElementById('dress11').innerHTML);
+                                            if(currentQuantity>0)
+                                            {
+                                                document.getElementById('dress11').innerHTML = --currentQuantity;
+                                            }
+                                        }
+                                        function inc11(){
+                                        
+                                            var currentQuantity= parseInt(document.getElementById('dress11').innerHTML);
+                                        
+                                            document.getElementById('dress11').innerHTML = ++currentQuantity;
+                                            }
+
+    // const productOptions = document.querySelectorAll(".card-order");
+    // productOptions.forEach((node) => {
+    //      const itemCount = node.querySelector(".qnt-dress");
  
-         const add = node.querySelector(".inc");
-         const remove = node.querySelector(".dec");
+    //      const add = node.querySelector(".inc");
+    //      const remove = node.querySelector(".dec");
  
-         add.addEventListener("click", () => {
-         //   itemCount.value = parseInt(itemCount.value, 10) + 1;
-         let currentQuantity= node.querySelector('.qnt-dress').innerHTML;
+    //      add.addEventListener("click", () => {
+    //      //   itemCount.value = parseInt(itemCount.value, 10) + 1;
+    //      let currentQuantity= node.querySelector('.qnt-dress').innerHTML;
  
-         node.querySelector('.qnt-dress').innerHTML = ++currentQuantity;
-         });
+    //      node.querySelector('.qnt-dress').innerHTML = ++currentQuantity;
+    //      });
        
-         remove.addEventListener("click", () => {
-         //   itemCount.value = parseInt(itemCount.value, 10) - 1;
-         let currentQuantity= node.querySelector('.qnt-dress').innerHTML;
-     if(currentQuantity>0)
-     {
-         node.querySelector('.qnt-dress').innerHTML = --currentQuantity;
-     }
-         });
-       });
+    //      remove.addEventListener("click", () => {
+    //      //   itemCount.value = parseInt(itemCount.value, 10) - 1;
+    //      let currentQuantity= node.querySelector('.qnt-dress').innerHTML;
+    //  if(currentQuantity>0)
+    //  {
+    //      node.querySelector('.qnt-dress').innerHTML = --currentQuantity;
+    //  }
+    //      });
+    //    });
 
   
 
